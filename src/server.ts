dec31140337fe790373d7354671530ed03b1014f
lucasmatos98 <cdxlucas@gmail.com>
2021-06-06 23:33:34 -0300

    feat: eslint

--- src/server.ts
+++ src/server.ts
@@ -2,31 +2,32 @@
 // where your node app starts
 
 // init project
-import express = require('express');
-var app = express();
+import * as express from 'express';
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+import * as cors from 'cors';
+import { AddressInfo } from 'net';
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-	res.sendFile(__dirname + '/views/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-	res.json({ greeting: 'hello API' });
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+    res.json({ greeting: 'hello API' });
 });
-
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-	console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT, function () {
+    const { port } = listener.address() as AddressInfo;
+    console.log('Your app is listening on port ' + port);
 });
